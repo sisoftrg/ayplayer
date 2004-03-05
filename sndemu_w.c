822f8b94d4101ed66fd584fb791506650146222c
root <root>
2004-03-05 14:46:09 +0000

    fix compiling.

--- sndemu_w.c
+++ sndemu_w.c
@@ -1,5 +1,5 @@
 //(c)2003 sisoft\trg - AYplayer.
-/* $Id: sndemu_w.c,v 1.2 2003/11/06 09:21:37 root Exp $ */
+/* $Id: sndemu_w.c,v 1.3 2004/03/05 14:46:09 root Exp $ */
 
 //base version of this file was taken from x128 emulator by James McKay.
 
@@ -11,16 +11,12 @@
 #include "z80.h"
 
 static _UC PSG[16];
-static _UC VolA=0,VolB=0,VolC=0;
 static _UC white_noise=1;
 static _UC Volumes[16]= {
         0xFF,0x3F,0x3F,0x3F,0x2C,0x26,0x22,0x1D,
         0x16,0x12,0x0C,0x0A,0x07,0x05,0x02,0x00
 };
 
-/*** PutAdlib ****************************************/
-/*** Write value V into Adlib R.          ***/
-/*****************************************************/
 static void PutAdlib(_UC R,_UC V)
 {
     _UC J;
@@ -28,9 +24,6 @@ static void PutAdlib(_UC R,_UC V)
     outp(0x389,V);for(J=0;J<35;J++) inp(0x388);
 }
 
-/*** MuteSound ***************************************/
-/*** Turn all sound off.                           ***/
-/*****************************************************/
 static void MuteSound(void)
 {
     _UC OldPSG7=255;
@@ -40,14 +33,9 @@ static void MuteSound(void)
     PSG[7]=OldPSG7;
 }
 
-/*** InitAdlib ***************************************/
-/*** Return 0 if Adlib is not found, otherwise     ***/
-/*** initialize it and return 1.                   ***/
-/*****************************************************/
 int sound_init()
 {
     _UC I,A1,A2;
-    /* If Adlib is not to be used, don't initialize */
     /********* Detect if Adlib is present. *************/
     PutAdlib(0x01,0x00);    /* Delete test    */
     PutAdlib(0x04,0x60);    /* Mask and disable timers */
@@ -60,7 +48,6 @@ int sound_init()
     PutAdlib(0x04,0x60);    /* Mask and disable timers */
     PutAdlib(0x04,0x80);    /* Reset timers            */
     if((A2!=0xC0)||A1)return(0);
-    /***************************************************/
     /* Allow the FM chips to control WaveForm */
     for(I=0;I<255;I++) PutAdlib(I,0x00);
     PutAdlib(0x01,0x20);
@@ -90,9 +77,6 @@ int sound_init()
     return(1);
 }
 
-/*** TrashAdlib **************************************/
-/*** If Adlib was used then shut it down.          ***/
-/*****************************************************/
 void sound_end()
 {
     MuteSound();         /* Turn off the sound      */
@@ -101,66 +85,6 @@ void sound_end()
 }
 
 void sound_ay_reset() {}
-
-/*** PlayA *******************************************/
-/*** Play sound via PSG channel A. Called when the ***/
-/*** registers related to this channel are changed ***/
-/*****************************************************/
-static void PlayA()
-{
-    _UC J;
-    _US Lath;
-    long Tune;
-    if(!(PSG[7]&0x01)) {
-        PutAdlib(0x43,Volumes[VolA]);
-        Lath=((_US)PSG[1]<<8)|PSG[0];
-        Lath&=4095; /* Only 12 bits required */
-        Tune=Lath? 2345678L/Lath:1;
-        for(J=0;Tune>0x0400;Tune>>=1) J++;
-        PutAdlib(0xA0,Tune&0xFF);
-        PutAdlib(0xB0,((Tune>>8)&0x03)|(J<<2)|0x20);
-    } else PutAdlib(0x43,Volumes[0]);
-}
-
-/*** PlayB *******************************************/
-/*** Play sound via PSG channel B. Called when the ***/
-/*** registers related to this channel are changed ***/
-/*****************************************************/
-static void PlayB()
-{
-    _UC J;
-    _US Lath;
-    long Tune;
-    if(!(PSG[7]&0x02)) {
-        PutAdlib(0x44,Volumes[VolB]);
-        Lath=((_US)PSG[3]<<8)|PSG[2];
-        Lath&=4095;
-        Tune=Lath? 2345678L/Lath:1;
-        for(J=0;Tune>0x0400;Tune>>=1) J++;
-        PutAdlib(0xA1,Tune&0xFF);
-        PutAdlib(0xB1,((Tune>>8)&0x03)|(J<<2)|0x20);
-    } else PutAdlib(0x44,Volumes[0]);
-}
-
-/*** PlayC *******************************************/
-/*** Play sound via PSG channel C. Called when the ***/
-/*** registers related to this channel are changed ***/
-/*****************************************************/
-static void PlayC()
-{
-    _UC J;
-    _US Lath;
-    long Tune;
-    if(!(PSG[7]&0x04)) {
-        PutAdlib(0x45,Volumes[VolC]);
-        Lath=((_US)PSG[5]<<8)|PSG[4];
-        Lath&=4095;
-        Tune=Lath? 2345678L/Lath:1;
-        for(J=0;Tune>0x0400;Tune>>=1) J++;
-        PutAdlib(0xA2,Tune&0xFF);
-        PutAdlib(0xB2,((Tune>>8)&0x03)|(J<<2)|0x20);
-    } else PutAdlib(0x45,Volumes[0]);
-}
 
 static void Set_Tone(_UC R)
 {
@@ -203,7 +127,7 @@ static void Set_Tone(_UC R)
         }
         break;
     }
-#ifdef IGNORE_BANANA
+//#ifdef IGNORE_BANANA
     switch(((PSG[7]>>R)&9)) {
     case 9: /* Neither */
         break;
@@ -222,7 +146,7 @@ static void Set_Tone(_UC R)
         PutAdlib(0xB7,((Tune>>8)&0x03)|(J<<2)|0x20);
         break;
     }
-#endif
+//#endif
 }
 
 static void Set_Vol(_UC R)
@@ -236,35 +160,8 @@ static void Set_Vol(_UC R)
     else if(white_noise) PutAdlib(0x54,Volumes[V]);
 }
 
-/*** PSGOut ******************************************/
-/*** Write value V into AY8910 R.         ***/
-/*** Appropriate sound routines are called as it   ***/
-/*** is done.                                      ***/
-/*****************************************************/
-
-#ifdef USING_NEW_PSGOUT
-void PSGOut(_UC R,_UC V)
-{
-    _UC R2, V2;
-    PSG[R]=V;
-    _disable();
-    switch(R) {
-        case 7: PlayA();PlayB();PlayC();PlayNoise();break;
-        case 6: PlayNoise();break;
-	case 11:case 12: break; /* No envelope support yet */
-	case 0: case 1: case 8: PlayA();break;
-        case 8: if(PSG[8]&0x10) VolA=15; else VolA=PSG[8]&0x0F;
-        	PlayA();break;
-	case 2: case 3: case 9: PlayB();break;
-	case 4: case 5: case 10: PlayC();break;
-    }
-    _enable();
-}
-#endif
-
 void sound_ay_write(int R,int V)
 {
-    _UC R2, V2;
     PSG[R]=(_UC)V;
     _disable();
     switch(R) {
